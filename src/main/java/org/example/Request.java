bb7857b10aa8bc3fe7836317b7b303cee31ce60d
Vera Pak <verapark.al@icloud.com>
2024-05-15 14:05:23 +0300

    Parse x-www-form-urlencoded in body

--- src/main/java/org/example/Request.java
+++ src/main/java/org/example/Request.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import org.apache.commons.collections4.MultiValuedMap;
 import org.apache.http.NameValuePair;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -10,13 +12,13 @@ public class Request {
     static Logger logger = MyLogger.getInstance().getLogger();
     private final String[] requestLine;
     private final List<String> headers;
-    private final String body;
+    private final MultiValuedMap<String, String> body;
 
     private final String method;
     private final String path;
     private final List<NameValuePair> queryParams;
 
-    public Request(String[] requestLine, String method, String path, List<String> headers, String body, List<NameValuePair> queryParams) {
+    public Request(String[] requestLine, String method, String path, List<String> headers, MultiValuedMap<String, String> body, List<NameValuePair> queryParams) {
         this.requestLine = requestLine;
         this.headers = headers;
         this.body = body;
@@ -33,7 +35,7 @@ public class Request {
         return headers;
     }
 
-    public String getBody() {
+    public MultiValuedMap<String, String> getBody() {
         return body;
     }
 
@@ -66,4 +68,25 @@ public class Request {
         logger.log(Level.WARNING, "Query-параметры не найден");
         return null;
     }
+
+    public Collection<String> getPostParam(String name){
+        if(body == null) {
+            logger.log(Level.WARNING, "Параметры отсутствуют в теле запроса");
+            return null;
+        }
+        if(!body.containsKey(name)) {
+            logger.log(Level.WARNING, "Параметр отсутствует в теле запроса");
+            return null;
+        }
+        return body.get(name);
+    }
+
+    public MultiValuedMap<String, String> getPostParams(){
+        if(body == null) {
+            logger.log(Level.WARNING, "Параметры отсутствуют в теле запроса");
+            return null;
+        }
+        return body;
+    }
+
 }
